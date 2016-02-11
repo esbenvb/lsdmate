06f23998a5e7396a910fad037614621f22649384
Esben von Buchwald <esben@vonbuchwald.dk>
2016-02-11 22:43:47 +0100

    Initial Commit

---
+++ LSDmate/LSDmate.h
@@ -0,0 +1,19 @@
+//
+//  LSDmate.h
+//  LSDmate
+//
+//  Created by Esben von Buchwald on 11/02/16.
+//  Copyright © 2016 Esben von Buchwald. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LSDmate.
+FOUNDATION_EXPORT double LSDmateVersionNumber;
+
+//! Project version string for LSDmate.
+FOUNDATION_EXPORT const unsigned char LSDmateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSDmate/PublicHeader.h>
+
+
