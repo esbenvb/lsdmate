9634bcdf71d220181c9e14eb2a691beb01286879
Esben von Buchwald <esben@vonbuchwald.dk>
2016-02-11 23:17:04 +0100

    initial commit

---
+++ LSDmate-GUI/LSDmate-GUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LSDmate-GUI
+//
+//  Created by Esben von Buchwald on 11/02/16.
+//  Copyright © 2016 Esben von Buchwald. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LSDmate-GUI/LSDmate-GUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LSDmate-GUI
+//
+//  Created by Esben von Buchwald on 11/02/16.
+//  Copyright © 2016 Esben von Buchwald. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
