269f0cc2d070d89165f067de13966aa0e591569f
Esben von Buchwald <esben@vonbuchwald.dk>
2016-02-18 23:59:47 +0100

    Created basic UI mockup

--- LSDmate-GUI/ViewController.swift
+++ LSDmate-GUI/ViewController.swift
@@ -10,12 +10,45 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    enum Mode: Int {
+        case Play = 0
+        case Work = 1
+    }
+    
+    var hosts = ["hej", "med", "dig"]
+    
+    @IBOutlet weak var modeSelector: NSSegmentedControl!
+    @IBOutlet weak var addHost: NSButton!
+    @IBOutlet weak var removeHost: NSButton!
+
+    @IBOutlet weak var hostsTable: NSTableView!
+
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        hostsTable.setDelegate(self)
+        hostsTable.setDataSource(self)
+        
     }
 
+    @IBAction func switchMode(sender: NSSegmentedControl) {
+       print(sender.selectedSegment)
+    }
+    
+    @IBAction func addHostClicked(sender: NSButton) {
+        reloadHosts()
+        print(hosts)
+
+    }
+    
+    @IBAction func removeHostClicked(sender: NSButton) {
+        let rows = hostsTable.selectedRowIndexes
+        let selectedHosts = rows.map({hosts[$0]})
+        print(selectedHosts)
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
@@ -23,5 +56,36 @@ class ViewController: NSViewController {
     }
 
 
+    func reloadHosts() {
+        hosts.append("aasdsa\(random())" )
+        hostsTable.reloadData()
+    }
 }
 
+
+extension ViewController : NSTableViewDataSource {
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return hosts.count
+    }
+}
+
+extension ViewController : NSTableViewDelegate {
+   /*func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let item = hosts[row]
+        
+    let cellIdentifier = "host"
+    let text = item
+    
+    if let cell = tableView.makeViewWithIdentifier(cellIdentifier, owner: self) as? NSTableCellView  {
+        cell.textField?.stringValue = text
+        return cell
+    }
+    return nil
+    }
+   
+*/
+    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        return hosts[row]
+    }
+
+}
